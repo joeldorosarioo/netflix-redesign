13e778c34edf9a931d29b9b65fd5d5de849245de
Joel Oliveira <joeldorosarioo@gmail.com>
2022-05-26 10:33:20 -0300

    feat: add initial setup

---
+++ src/pages/_document.tsx
@@ -0,0 +1,26 @@
+import { Head, Html, Main, NextScript } from "next/document";
+
+export default function Document () {
+	return (
+		<Html lang="en">
+			<Head>
+				<meta name="description" content="A Netflix redesign for learning and pratice purposes, using NextJS, TypeScript and TMDB API." />
+
+				<link rel="shortcut icon" href="favicon.png" type="image/png" />
+
+				<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap" />
+
+				<link rel="preconnect" href="https://fonts.googleapis.com" />
+				<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+
+				<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" />
+				<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,300;0,400;1,300;1,400&display=swap" />
+			</Head>
+
+			<body>
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	)
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Head from 'next/head';
+
+export default function Home () {
+	return (
+		<React.Fragment>
+			<Head>
+				<title>Netflix - Assista a Séries e Filmes On-line</title>
+			</Head>
+
+			<h1>Netflix Redesign</h1>
+		</React.Fragment>
+	)
+}

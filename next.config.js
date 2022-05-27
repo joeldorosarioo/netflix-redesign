c4d2384eb4aea5b01919bfb45adcc2864cb2a201
Joel Oliveira <joeldorosarioo@gmail.com>
2022-05-27 09:47:56 -0300

    feat: setting the environment variable in next.config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
+
+	env: {
+		PUBLIC_API_KEY: process.env.PUBLIC_API_KEY,
+	},
 }
 
 module.exports = nextConfig

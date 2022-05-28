5a72fdf6bd2b34c283f4bafdf111bd0eb8e9633b
Joel Oliveira <joeldorosarioo@gmail.com>
2022-05-28 15:48:31 -0300

    feat: creating featured movie component

---
+++ src/components/FeaturedMovie.tsx
@@ -0,0 +1,48 @@
+import IMovieCommon from '../interfaces/IMovieCommon';
+
+import styles from '../stylesheets/components/FeaturedMovie.module.css';
+
+type FeaturedMovieProps = {
+  movie: IMovieCommon
+}
+
+export default function FeaturedMovie({ movie }: FeaturedMovieProps) {
+	const regExpFirstAirDate = new RegExp('^[0-9]{3}[a-zA-Z0-9]', 'g');
+
+	return (
+		<section className={ styles.featuredMovie } style={{
+			backgroundImage:
+				`linear-gradient(360deg, #111111, rgba(17, 17, 17, .46)), url(https://image.tmdb.org/t/p/original/${ movie?.backdrop_path })`
+		}}>
+			<div className={ styles.movieInfo }>
+				<h1 className={ styles.movieTitle }>{ movie?.name }</h1>
+
+				<div className={ styles.wrapper }>
+					<span className={ styles.voteAverage }>{ Math.round((100 * movie?.vote_average) / 10) }% Match</span>
+					<span className={ styles.firstAirDate }>{ regExpFirstAirDate.exec(movie?.first_air_date) }</span>
+
+					<span className={ styles.numberOfSeasons }>
+						{ movie?.number_of_seasons } Season{ movie?.number_of_seasons !== 1 ? 's' : '' }
+					</span>
+				</div>
+
+				<div className={ styles.movieDescription }>{ movie?.overview }</div>
+
+				<div className={ styles.buttonsWrapper }>
+					<button type="button" className={ styles.watchButton }>► Play</button>
+					<button type="button" className={ styles.myListButton }>+ Watch List</button>
+				</div>
+
+				<div className={ styles.movieGenres }>
+					<strong>Gêneros:</strong> { movie?.genres.map((gender: any, index: number) => (
+						index !== movie?.genres.length - 1 ? (
+							<span key={ gender.id }>{`${ gender.name }, `}</span>
+						) : (
+							<span key={ gender.id }>{ gender.name }</span>
+						)
+					)) }
+				</div>
+			</div>
+		</section>
+	)
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Head from 'next/head';
 
+import IMovieCommon from '../interfaces/IMovieCommon';
+
 import Header from '../components/Header';
+import FeaturedMovie from '../components/FeaturedMovie';
+
+import TMDB from '../utils/Tmdb';
 
 export default function Home () {
+	const [movieList, setMovieList] = useState([]);
+	const [featuredMovie, setFeaturedMovie] = useState<IMovieCommon>();
+
+	useEffect(() => {
+		(async () => {
+			let list: any = await TMDB.getAllData();
+			let originals = list.filter((i: { slug: string; }) => i.slug === 'originals');
+
+			let randomMovie = Math.floor(Math.random() * (originals[0].items.results.length - 1));
+			let featuredMovie: IMovieCommon = await TMDB.getMovieInfo(originals[0].items.results[randomMovie].id, 'tv');
+
+			setMovieList(list);
+			setFeaturedMovie(featuredMovie);
+		})();
+	}, []);
+
 	return (
 		<React.Fragment>
 			<Head>
@@ -11,6 +32,10 @@ export default function Home () {
 			</Head>
 
 			<Header />
+
+
+			{ featuredMovie && <FeaturedMovie movie={ featuredMovie } /> }
+
 		</React.Fragment>
 	)
 }

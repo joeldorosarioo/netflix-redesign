1cb14869315025777434dbe3bed9d5887cc30ae9
Joel Oliveira <joeldorosarioo@gmail.com>
2022-05-29 18:12:05 -0300

    feat: adding react-slick library in project

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -15,6 +15,9 @@ export default function Document () {
 
 				<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" />
 				<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,300;0,400;1,300;1,400&display=swap" />
+
+				<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
+				<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
 			</Head>
 
 			<body>
